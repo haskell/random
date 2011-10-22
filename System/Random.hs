2d9439e81640bb0b0eab933354c8f961f34cffa3
Ryan Newton <rrnewton@gmail.com>
2011-10-22 06:01:00 -0400

    Minor: change the comments to refer to the correct LICENSE file.

--- System/Random.hs
+++ System/Random.hs
@@ -6,7 +6,7 @@
 -- |
 -- Module      :  System.Random
 -- Copyright   :  (c) The University of Glasgow 2001
--- License     :  BSD-style (see the file libraries/base/LICENSE)
+-- License     :  BSD-style (see the file LICENSE in the 'random' repository)
 -- 
 -- Maintainer  :  libraries@haskell.org
 -- Stability   :  stable
